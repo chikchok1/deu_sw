aff2189272ca217bfc6f3da01f694d60c1385adf
chikchok <tony09088@naver.com>
2025-04-11 14:11:00 +0900

    test

---
+++ ClassReservation/src/main/java/deu/CRS/Login/ClassReservation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package deu.CRS.Login;
+
+/**
+ *
+ * @author YangJinWon
+ */
+public class ClassReservation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
